f078309cefbc32e8401f0a1bdd941da4c8ee805d
Minh <66846878+minhphan03@users.noreply.github.com>
2021-06-17 15:44:53 -0700

    Create binary-heap.cpp

---
+++ trees/binary-heap.cpp
@@ -0,0 +1,68 @@
+template<typename T>
+class binary_max_heap {
+public:
+    std::size_t size() { return data_.size(); }
+    bool empty() { return data_.empty(); }
+
+    void insert(const T& value) {
+        data_.push_back(value);
+
+        // cascade up
+        auto n = data_.size() - 1;
+        auto parent = get_parent(n);
+        while (n > 0 && !(data_[parent] > data_[n])) {
+            std::swap(data_[n], data_[parent]);
+            n = parent;
+            parent = get_parent(n);
+        }
+    }
+
+    void remove() {
+        std::swap(data_[0], data_[data_.size() - 1]);
+        data_.pop_back();
+        
+        // cascade down
+        std::size_t n = 0;
+        auto left = get_left_child(n);
+        auto right = get_right_child(n);
+        while ((left < data_.size() && !(data_[n] > data_[left])) || (right < data_.size() && !(data_[n] > data_[right]))) {
+            // find the largest child
+            auto largest = left;
+            if ((right < data_.size()) && (data_[right] > data_[largest])) {
+                largest = right;
+            }
+
+            // swap with larger child
+            std::swap(data_[n], data_[largest]);
+            n = largest;
+            left = get_left_child(n);
+            right = get_right_child(n);
+        }
+    }
+
+    T& root() { return data_.front(); }
+
+private:
+    std::vector<T> data_;
+
+    std::size_t get_left_child(std::size_t n) { return 2 * n + 1; }
+    std::size_t get_right_child(std::size_t n) { return 2 * n + 2; }
+    std::size_t get_parent(std::size_t n) { return (n - 1) / 2; }
+};
+// ...
+int main() {
+    std::mt19937 rng{ std::random_device{}() };
+    std::uniform_int_distribution range{ 0, 100 };
+
+    my::binary_max_heap<int> heap;
+    for (int i = 0; i < 20; ++i) {
+        heap.insert(range(rng));
+    }
+
+    std::cout << "HEAP: ";
+    while (!heap.empty()) {
+        std::cout << heap.root() << ", ";
+        heap.remove();
+    }
+    std::cout << "\n";
+}
